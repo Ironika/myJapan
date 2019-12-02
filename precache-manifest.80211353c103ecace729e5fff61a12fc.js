bf255126a896e794fdf89fff7a7a30dd234cc9bb
Kévin Hoquet <kevin.hoquet@digitaslbi.fr>
2019-12-02 17:44:13 +0100

    Updates

---
+++ precache-manifest.80211353c103ecace729e5fff61a12fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7133d36bd2d7dceb6c0885037f09c588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2437479d842de857897d",
+    "url": "/static/css/main.558d6534.chunk.css"
+  },
+  {
+    "revision": "467ea45d0e9515200986",
+    "url": "/static/js/2.5ca209f4.chunk.js"
+  },
+  {
+    "revision": "2437479d842de857897d",
+    "url": "/static/js/main.2b2e469c.chunk.js"
+  },
+  {
+    "revision": "29f2520dd871461a8e6d",
+    "url": "/static/js/runtime-main.f9df6959.js"
+  },
+  {
+    "revision": "7c8d63fd2819ce371d3b32fd16fb493e",
+    "url": "/static/media/adala.7c8d63fd.png"
+  },
+  {
+    "revision": "7395ebb6668a192d34828182cb189851",
+    "url": "/static/media/banner.7395ebb6.jpg"
+  },
+  {
+    "revision": "ef6ecf9b748d52e32d6824c37518a98d",
+    "url": "/static/media/cerisier.ef6ecf9b.png"
+  },
+  {
+    "revision": "4f6a802ddbe66b144c7ab808f3b0fcd0",
+    "url": "/static/media/homescreen.4f6a802d.jpg"
+  },
+  {
+    "revision": "7933a83d2da028571bdcdede9116a401",
+    "url": "/static/media/homescreen2.7933a83d.jpg"
+  },
+  {
+    "revision": "a36ad686ca84ba1b6db0c75310993c05",
+    "url": "/static/media/homescreen3.a36ad686.jpg"
+  },
+  {
+    "revision": "7b3f4161157f9e353d8a1099ceab340d",
+    "url": "/static/media/loader.7b3f4161.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://ironika.github.io/myJapan/precache-manifest.e9c3dfd8c3b9822d53faba09967a65cd.js"
+  "/precache-manifest.80211353c103ecace729e5fff61a12fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://ironika.github.io/myJapan/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
